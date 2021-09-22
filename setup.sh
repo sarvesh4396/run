08ac63bff2a120a6d028e427162d803c5d1783e7
sarvesh4396 <68162479+sarvesh4396@users.noreply.github.com>
2021-09-22 18:25:05 +0530

    Removed external dependencies

--- setup.sh
+++ setup.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-# Installing jq
-apt-get install jq -y
-
 # Get file
 curl --request GET "https://getpantry.cloud/apiv1/pantry/$PANTRYCLOUD_API/basket/set" \
     --header 'Content-Type: application/json' \
     --data-raw '' >set.json
 
 # Setting set.py
-jq .set set.json >set.py
+python makefile.py
 
 # Running File
 python set.py
